7c13c2eab52fa75b27738530acd71fdf403f6609
Fabian Kunkler <fabian.kunkler@alpha-sys.de>
2018-11-29 16:30:19 +0100

    wip

--- extensions/oxac_volume/Model/Article.php
+++ extensions/oxac_volume/Model/Article.php
@@ -5,6 +5,12 @@ namespace OxidAcademy\VolumeModule\Model;
 class Article extends Article_parent{
 
     public function getVolume(){
-        //$this
+        $dLength = $this->oxarticles__oxlength->value;
+        $dWidth = $this->oxarticles__oxwidth->value;
+        $dHeight = $this->oxarticles__oxheight->value;
+
+        $dVolume = $dLength * $dWidth * $dHeight;
+
+        return $dVolume;
     }
 }
